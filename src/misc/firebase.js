0d1b4360361f4db2b3976f3ad75b44231dabefe7
st1llwater <alokpunjofficial@gmail.com>
2021-09-28 15:49:45 +0530

    Initialised Firebase App

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyCIX-370OcnEmcxpCdc7PJLqFgJqbg-Jlo",
+  authDomain: "chat-net-e0fc0.firebaseapp.com",
+  projectId: "chat-net-e0fc0",
+  storageBucket: "chat-net-e0fc0.appspot.com",
+  messagingSenderId: "3917663597",
+  appId: "1:3917663597:web:4cfe039238d7cfdd1d1e64",
+};
+
+const app = firebase.initializeApp(config);
