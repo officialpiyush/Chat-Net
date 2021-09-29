328e12331baf970895aebc4daae97ff57073fe2c
st1llwater <alokpunjofficial@gmail.com>
2021-09-29 15:17:02 +0530

    Scaffolding And Layouting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
-import "rsuite/dist/rsuite.min.css";
+import { Switch } from "react-router";
+import "rsuite/dist/styles/rsuite-default.css";
+import { Route } from "workbox-routing";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
 import "./styles/main.scss";
 
 function App() {
-  return <div className="App">GG</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
