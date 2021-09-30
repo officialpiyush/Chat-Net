46c395d2f4ce851eb25f773a2012cfe8de46eb3b
Alok <alokpunjofficial@gmail.com>
2021-09-30 17:23:47 +0530

    Sign In Connections

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,16 +1,23 @@
+// Import the functions you need from the SDKs you need
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const config = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: 'AIzaSyCIX-370OcnEmcxpCdc7PJLqFgJqbg-Jlo',
   authDomain: 'chat-net-e0fc0.firebaseapp.com',
+  databaseURL:
+    'https://chat-net-e0fc0-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-net-e0fc0',
   storageBucket: 'chat-net-e0fc0.appspot.com',
   messagingSenderId: '3917663597',
   appId: '1:3917663597:web:4cfe039238d7cfdd1d1e64',
 };
 
-const app = firebase.initializeApp(config);
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
 export const auth = app.auth();
 export const database = app.database();
